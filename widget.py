d1b581771b81b9aff55f2cad97b8dc7d1a0e8a18
Marco Coppola <38087157+Murkrow02@users.noreply.github.com>
2024-07-19 13:47:35 +0200

    Update widget.py

--- widget.py
+++ widget.py
@@ -1,7 +1,8 @@
 from mosaico import widget, config
 
 # Create image
-img = widget.createImage(widget.configAsset("image"))
+img = widget.createImage()
+img.setImage(widget.configAsset("image"))
 
 def loop():
     pass
